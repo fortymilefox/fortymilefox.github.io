50f31ddbef5b22d4d98b0d64d0605bb2743b6b12
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-03-26 14:24:15 -0400

    update scripts
    Fixed all linter errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var pokemonRepository = (function() {
   //IIFE
   var repository = [];
-  var apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(newPokemon) {
     repository.push(newPokemon);
@@ -12,87 +12,79 @@ var pokemonRepository = (function() {
   }
 
   function addListItem(pokemon) {
-    var $pokeList = $(".pokemon-list"); // the ul from HTML
+    var $pokeList = $('.pokemon-list'); // the ul from HTML
     var $button = $(
       '<button type="button" class = "btn btn-info btn-lg btn-sm button-class list-group-item" data-target="#exampleModal" data-toggle="modal">' +
         pokemon.name +
-        "</button>"
+        '</button>'
     );
-    var $listItem = $("<li></li>");
+    var $listItem = $('<li></li>');
     $listItem.append($button);
     $pokeList.append($listItem);
-    $button.on("click", function() {
+    $button.on('click', function() {
       showDetails(pokemon);
     });
   }
 
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function() {
-      console.log(item);
       showModal(item);
     });
   }
 
   function loadList() {
-    return $.ajax(apiUrl, { dataType: "json" })
-      .then(function(json) {
-        json.results.forEach(function(item) {
-          var pokemon = {
-            name: item.name,
-            detailsUrl: item.url
-          };
-          add(pokemon);
-          console.log(pokemon);
-        });
-      })
-      .catch(function(e) {
-        console.error(e);
+    return $.ajax(apiUrl, { dataType: 'json' }).then(function(json) {
+      json.results.forEach(function(item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
       });
+    });
   }
 
   function loadDetails(item) {
     // Loading the details from the API
     var url = item.detailsUrl;
-    return $.ajax(url, { dataType: "json" })
-      .then(function(details) {
-        //Now we add the details to the items
-        item.imageUrl = details.sprites.front_default;
-        item.height = details.height;
-        item.weight = details.weight;
-        // item.types = Object.keys(details.types); //this returns an array of details
-        item.types = []; // Loop to go through the types and add them, if there's more than 1
-        for (var i = 0; i < details.types.length; i++) {
-          item.types.push(details.types[i].type.name);
-        }
-        item.abilities = [];
-        for(var i = 0; i < details.abilities.length; i++){
-          item.abilities.push(details.abilities[i].ability.name);
-        }
-      })
-      .catch(function(e) {
-        console.error(e);
-      });
+    return $.ajax(url, { dataType: 'json' }).then(function(details) {
+      //Now we add the details to the items
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.weight = details.weight;
+      // item.types = Object.keys(details.types); //this returns an array of details
+      item.types = []; // Loop to go through the types and add them, if there's more than 1
+      for (var i = 0; i < details.types.length; i++) {
+        item.types.push(details.types[i].type.name);
+      }
+      item.abilities = [];
+      for (var k = 0; k < details.abilities.length; k++) {
+        item.abilities.push(details.abilities[i].ability.name);
+      }
+    });
   }
 
   function showModal(item) {
-    var modalBody = $(".modal-body");
-    var modalTitle = $(".modal-title");
+    var modalBody = $('.modal-body');
+    var modalTitle = $('.modal-title');
     modalBody.empty();
+    ('.modal-body');
     modalTitle.empty();
+    ('.modal-title');
 
     //   // Pokemon's name
-    var nameElement = $("<h2>" + item.name + "</h2>");
+    var nameElement = $('<h2>' + item.name + '</h2>');
     //   //Pokemon's height
-    var heightElement = $("<p>" + "Height: " + item.height + "</p>");
+    var heightElement = $('<p>' + 'Height: ' + item.height + '</p>');
     //  //Pokémon's weight
-    var weightElement = $("<p>" + "Weight: " + item.weight + "</p>");
+    var weightElement = $('<p>' + 'Weight: ' + item.weight + '</p>');
     //   // Image
     var imageElement = $('<img class="modal-img">');
-    imageElement.attr("src", item.imageUrl);
+    imageElement.attr('src', item.imageUrl);
     //   // Pokemon's types
-    var typesElement = $("<p>" + "Types: " + item.types + "</p>");
+    var typesElement = $('<p>' + 'Types: ' + item.types + '</p>');
     //  // Pokémon's abilities
-    var abilitiesElement = $("<p>" + "Abilities: " + item.abilities + "</p>");
+    var abilitiesElement = $('<p>' + 'Abilities: ' + item.abilities + '</p>');
     //
 
     modalTitle.append(nameElement);
@@ -123,11 +115,11 @@ pokemonRepository.loadList().then(function() {
 
 // Search bar functionality
 $(document).ready(function() {
-  $("#myInput").on("keyup", function() {
+  $('#myInput').on('keyup', function() {
     var value = $(this)
       .val()
       .toLowerCase();
-    $("#myDIV *").filter(function() {
+    $('#myDIV *').filter(function() {
       $(this).toggle(
         $(this)
           .text()
